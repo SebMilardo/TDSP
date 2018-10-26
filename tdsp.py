a37852bb47a041d3fd265721f33de6c3ed61e95f
Sebastiano Milardo <s.milardo@hotmail.it>
2018-10-26 04:30:34 -0400

    Update tdsp.py

--- tdsp.py
+++ tdsp.py
@@ -1,6 +1,7 @@
 import numpy as np
 from Queue import PriorityQueue
-
+import matplotlib
+matplotlib.use("tkagg")
 
 class Graph(object):
     def __init__(self, vertices, edges, weights):
@@ -16,11 +17,13 @@ class Pair(object):
         self.v = v
 
     def __cmp__(self, other):
-        return cmp(self.g[self.tau], other.g[other.tau])
+        return cmp(self.g[self.tau[self.v]], other.g[other.tau[other.v]])
 
     def __str__(self):
-        return str((self.v, self.tau, self.g[self.tau]))
+        return str((self.v, self.tau[self.v], self.g[self.tau[self.v]]))
 
+    def __repr__(self):
+        return str((self.v, self.tau[self.v], self.g[self.tau[self.v]]))
 
 # Given v_s and v_e and a time window T, find the best time within T to depart from v_s and the path along which one
 # can arrive at v_e with the minimum travel time
@@ -40,7 +43,7 @@ class Pair(object):
 
 def w1_3(t):
     if 0 <= t < 5:
-        return 0
+        return 5
     if t >= 10:
         return 25
     if 5 <= t < 10:
@@ -80,27 +83,34 @@ def time_refinement(Gt, vs, ve, T):
                 g[v][t] = np.inf
             tau[v] = ts
 
-    Q_ = [Pair(tau[i], g[i], i) for i in Gt.vertices]
+    Q_ = [Pair(tau, g[i], i) for i in Gt.vertices]
     Q = PriorityQueue()
     for p in Q_:
         Q.put(p)
 
+    times = 0
     while len(Q.queue) >= 2:
+        print Q.queue
+        times = times +1
+        print times
         pair_i = Q.get()
-        pair_k = Q.queue[0]
-        tmp1 = [Gt.weights[e](pair_k.g[pair_k.tau]) for e in Gt.edges if e[1] == pair_i.v]
+        print pair_i
+        pair_k = Q.get()
+        Q.put(pair_k)
+        tmp1 = [Gt.weights[e](pair_k.g[pair_k.tau[pair_k.v]]) for e in Gt.edges if e[1] == pair_i.v]
         if len(tmp1) > 0:
             delta = np.min(tmp1)
         else:
             delta = np.inf
 
-        tau_i_first = np.max([t for t in T if pair_i.g[t] <= pair_k.g[pair_k.tau] + delta])
+        tau_i_first = np.max([t for t in T if pair_i.g[t] <= pair_k.g[pair_k.tau[pair_k.v]] + delta])
 
         for e in [e for e in Gt.edges if e[0] == pair_i.v]:
+            print ("updating",e[0],e[1])
             gj_first = dict()
-            for t in range(pair_i.tau, tau_i_first + 1):
+            for t in range(pair_i.tau[pair_i.v], tau_i_first + 1):
                 gj_first[t] = pair_i.g[t] + Gt.weights[e](t)
-            for t in range(pair_i.tau, tau_i_first + 1):
+            for t in range(pair_i.tau[pair_i.v], tau_i_first + 1):
                 g[e[1]][t] = min(g[e[1]][t], gj_first[t])
 
             # reorder Q
@@ -109,13 +119,13 @@ def time_refinement(Gt, vs, ve, T):
                 tmpQ.put(p)
             Q = tmpQ
 
-        tau_i = tau_i_first
+        tau[pair_i.v] = tau_i_first
 
-        if tau_i >= te:
+        if tau[pair_i.v] >= te:
             if pair_i.v == ve:
                 return g
         else:
-            Q.put(Pair(tau_i, pair_i.g, pair_i.v))
+            Q.put(Pair(tau, pair_i.g, pair_i.v))
     return g
 
 
@@ -133,6 +143,8 @@ def path_selector(Gt, g, vs, ve, t_star):
 
 def algorithm1(Gt, vs, ve, T):
     g = time_refinement(Gt, vs, ve, T)
+    for g_ in g.values():
+        pd.DataFrame(g_, index=[0]).T.plot()
     if sum(np.isinf(g[ve].values())) == 0:
         t_star = np.argmin([g[ve][t] - t for t in T])
         p_star = path_selector(Gt, g, vs, ve, t_star)
@@ -141,4 +153,4 @@ def algorithm1(Gt, vs, ve, T):
         return None
 
 
-print algorithm1(Gt, 1, 4, range(0, 60))
+print algorithm1(Gt, 1, 4, range(0, 61))

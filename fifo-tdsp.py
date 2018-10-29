e3133ed7d9a03437f176ae6446090d0009056714
Sebastiano Milardo <s.milardo@hotmail.it>
2018-10-29 14:38:49 -0400

    Add some tests and plots

--- fifo-tdsp.py
+++ fifo-tdsp.py
@@ -1,8 +1,12 @@
-import numpy as np
+import random
+import time
 from Queue import PriorityQueue
+
 import matplotlib
+import numpy as np
 
 matplotlib.use("tkagg")
+import pandas as pd
 
 
 class Graph(object):
@@ -10,6 +14,16 @@ class Graph(object):
         self.vertices = vertices
         self.edges = edges
         self.weights = weights
+        self.in_adj = list()
+        self.out_adj = list()
+
+        for v in vertices:
+            self.in_adj.append(list())
+            self.out_adj.append(list())
+
+        for e in edges:
+            self.in_adj[e[1]].append(e)
+            self.out_adj[e[0]].append(e)
 
 
 class Pair(object):
@@ -54,11 +68,11 @@ def time_refinement(Gt, vs, ve, T):
         pair_i = Q.get()
         pair_k = Q.get()
         Q.put(pair_k)
-        tmp = [Gt.weights[e](g[pair_k.v][tau[pair_k.v]]) for e in Gt.edges if e[1] == pair_i.v]
+        tmp = [Gt.weights[e](g[pair_k.v][tau[pair_k.v]]) for e in Gt.in_adj[pair_i.v]]
         delta = np.min(tmp) if len(tmp) > 0 else np.inf
         tau_i_first = np.max([t for t in T if g[pair_i.v][t] <= (g[pair_k.v][tau[pair_k.v]] + delta)])
 
-        for e in [e for e in Gt.edges if e[0] == pair_i.v]:
+        for e in Gt.out_adj[pair_i.v]:
             gj_first = dict()
             for t in range(pair_i.tau[pair_i.v], tau_i_first + 1):
                 gj_first[t] = pair_i.g[t] + Gt.weights[e](pair_i.g[t])
@@ -83,7 +97,7 @@ def path_selector(Gt, g, vs, ve, t_star):
     vj = ve
     p_star = list()
     while vj != vs:
-        for e in [e for e in Gt.edges if e[1] == vj]:
+        for e in Gt.in_adj[vj]:
             if g[e[0]][t_star] + Gt.weights[e](g[e[0]][t_star]) == g[vj][t_star]:
                 vj = e[0]
                 break
@@ -121,7 +135,7 @@ if __name__ == "__main__":
     # Weights:
     # the weight of an edge v(i,j) depends on time t and it is defined by the function wi_j
 
-    def w1_3(t):
+    def w0_2(t):
         if 0 <= t < 5:
             return 5
         if t >= 10:
@@ -130,24 +144,54 @@ if __name__ == "__main__":
             return 4 * t - 15
 
 
+    def w0_1(t):
+        return 10
+
+
     def w1_2(t):
         return 10
 
 
-    def w2_3(t):
-        return 10
-
-
-    def w2_4(t):
+    def w1_3(t):
         return 25
 
 
-    def w3_4(t):
+    def w2_3(t):
         return 10 if t >= 40 else 42 - 4.0 / 5.0 * t
 
 
-    T = range(0, 61)
-    weights = {(1, 3): w1_3, (2, 3): w2_3, (1, 2): w1_2, (2, 4): w2_4, (3, 4): w3_4}
-    Gt = Graph({1: 1, 2: 2, 3: 3, 4: 4}, [(1, 3), (2, 3), (1, 2), (2, 4), (3, 4)], weights)
+    start_time = time.time()
 
-    print algorithm(Gt, 1, 4, T)
+    T = range(0, 61)
+    vertices = {0: 0, 1: 1, 2: 2, 3: 3}
+    vs = 0
+    ve = 3
+    edges = [(0, 2), (1, 2), (0, 1), (1, 3), (2, 3)]
+    weights = {(0, 2): w0_2, (1, 2): w1_2, (0, 1): w0_1, (1, 3): w1_3, (2, 3): w2_3}
+    Gt = Graph(vertices, edges, weights)
+    print algorithm(Gt, vs, ve, T)
+
+    print time.time() - start_time
+
+    res = []
+    for ii in range(1, 100):
+        T2 = range(0, ii)
+        vertices2 = dict()
+        for i in range(ii): vertices2[i] = i
+        l = len(vertices2)
+        edges2 = [(i, j) for i in range(l) for j in range(l) if random.random() < 0.5]
+        weights2 = dict()
+        all_w = [w0_2, w1_2, w0_1, w1_3, w2_3]
+        for e in edges2:
+            weights2[e] = all_w[random.randint(0, 4)]
+        Gt = Graph(vertices2, edges2, weights2)
+
+        start_time = time.time()
+        p = algorithm(Gt, random.randint(0, ii - 1), random.randint(0, ii - 1), T2)
+        end = time.time() - start_time
+        item = [ii, end, len(vertices2), len(edges2), len(T2)]
+        print (p, item)
+        res.append(item)
+
+    df = pd.DataFrame(res, columns=["i", "time_elapsed", "n", "m", "T"])
+    df["time_elapsed"].plot()
